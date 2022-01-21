8d7bef930f516969471ca1cdc415ba9649699e22
Tomas <tomasgarciagonzalo@gmail.com>
2022-01-20 22:51:39 -0400

    Con tooltip y falta poco

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+$(function(){
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    });
+
+});
+/*
+$(function(){
+
+    $(function () {
+
+        $('[data-toggle="tooltip"]').tooltip()
+
+      })
+
+
+
+});*/
